ed25e0c069bb4b26ee719be01a52a02292da349b
Bruno Carreira <alfa.prime@gmail.com>
2012-11-30 16:54:09 +0000

    publico

---
+++ src/feeds/publico.js
@@ -0,0 +1,37 @@
+var config    	= require('../config/config.js').config,
+	rss 		= require('../lib/RSSReader.js'),
+    Gearman   	= require('node-gearman'),
+    gearClient 	= new Gearman(config.gearman.host, config.gearman.port),
+    winston   	= require('winston'),
+    feed_url 	= config.feed.source.publico,
+    last_link 	= undefined;
+
+var load_feed = function(){
+	winston.info("read feed: " + feed_url);
+	rss.parseURL(feed_url, function(articles) {
+
+	    winston.info("number of articles: " + articles.length);
+	    for(i=0; i<articles.length; i++) {
+	    	if (articles[i].origlink === last_link){
+	    		winston.info("nothing more to process");
+	    		break; // allready have process to this point
+	    	} 
+			if (typeof articles[i].origlink === 'string') {
+				winston.info("send page to scrap to queue " + articles[i].origlink);
+				var category = articles[i].category ? articles[i].category.toLowerCase() : undefined;
+				var data = {url: articles[i].origlink, tags: [ category ]};
+				var job = gearClient.submitJob('scraper', JSON.stringify(data))
+				job.on('error', function(err){
+					winston.error(err);
+				}).on('end', function(){
+					winston.info("job done");
+				});
+			}
+	    }
+	    last_link = articles[0].origlink;
+	    articles = undefined;
+	});
+	setTimeout(load_feed, config.feed.refresh);
+};
+load_feed();
+
